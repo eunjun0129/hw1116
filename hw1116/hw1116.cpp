edc63ab7d82dfd4419f64a02c0532e501e9921ee
eunjun0129 <eunjun0129@naver.com>
2023-11-16 03:46:55 +0900

    초기 커밋.

---
+++ hw1116/hw1116.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include "health.h"
+using namespace std;
+int main() {
+	CHealth VIP2(60), * mickey;  // need for CHealth() to be public 
+	mickey = new CHealth(20);
+
+	cout << "  " << VIP2.age << "  " << mickey->age << endl;
+
+	VIP2.measurefaren();
+	mickey->measureCel();
+
+	cout << "  " << mickey->cel << "C  " << VIP2.faren << "F \n";
+	cout << "  size " << sizeof(CHealth) << "\n";
+	getchar();
+	return 1;
+}
